721b2d0030bf07e309bf06c719a3e94782bdf150
Mohammad Moradi <mohammad.moradi9375@gmail.com>
2021-01-16 11:10:20 +0330

    Add ES5 files

---
+++ ES(EcmaScript)/ES5/1-string.js
@@ -0,0 +1,25 @@
+/**
+ * New Features:
+ *
+ *  - String.trim()
+ *  - Multiline String
+ */
+
+// ===========================
+//       String.Trim()
+// ===========================
+const text = "       Hello World!        "
+
+console.log({ beforeTrim: text })
+console.log({ afterTrim: text.trim() })
+
+// ===========================
+//      Multiline String
+// ===========================
+
+// const text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.\
+//  Voluptas voluptatibus fugit esse deleniti quae veritatis repudiandae\
+//  nesciunt eaque quibusdam laborum, asperiores quod corporis. Et perspiciatis,\
+//  incidunt omnis a velit nesciunt.'
+
+// console.log({ text })
---
+++ ES(EcmaScript)/ES5/5-strict-mode.js
@@ -0,0 +1,29 @@
+/**
+ * Strict mode makes several changes to normal JavaScript semantics:
+ *
+ *  *** Strict mode !== Sloppy mode ***
+ *
+ *  1- Eliminates some JavaScript silent errors by changing them to throw errors.
+ *  2- Fixes mistakes that make it difficult for JavaScript engines to perform optimizations:
+ *     strict mode code can sometimes be made to run faster than identical code that's not strict mode.
+ *  3- Prohibits some syntax likely to be defined in future versions of ECMAScript.
+ */
+
+// ===========================
+//      Strict mode levels
+// ===========================
+
+// Script
+// Function
+// Module
+
+// ===========================
+//          Examples
+// ===========================
+
+/*
+ - Global variable
+ - Delete function
+ - Change read-only properties
+ - Use `eval` or `arguments` as a variable
+*/
