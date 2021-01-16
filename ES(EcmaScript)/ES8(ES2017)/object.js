170e338c6901f988eecc7982ac460d955090ffb4
Mohammad Moradi <mohammad.moradi9375@gmail.com>
2021-01-16 15:05:56 +0330

    Add ES8 files

---
+++ ES(EcmaScript)/ES8(ES2017)/object.js
@@ -0,0 +1,17 @@
+/**
+ * New Features:
+ *  - Object.values()
+ *  - Object.entries()
+ */
+
+// ===========================
+//       Object.values()
+//       Object.entries()
+// ===========================
+const names = { firstName: 'turbo', lastName: 'front', company: 'part' }
+
+console.log({
+  values: Object.values(names),
+  entries: Object.entries(names)
+})
+
