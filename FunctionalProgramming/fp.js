a7d539f1842a3de848e5884c0b12cc2a30d43ec0
Mohammad Moradi <mohammad.moradi9375@gmail.com>
2021-01-18 15:19:28 +0330

    Add FP files

---
+++ FunctionalProgramming/fp.js
@@ -0,0 +1,14 @@
+/**
+ * Functional programming (often abbreviated FP) is the process of building software by composing pure functions,
+ * avoiding shared state, mutable data, and side-effects.
+ *
+ * Contrast with object oriented programming,
+ * where application state is usually shared and colocated with methods in objects.
+ */
+
+
+//# Pure functions
+//# Function composition
+//# Avoid shared state
+//# Avoid mutating state
+//# Avoid side effects
